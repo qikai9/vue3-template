d7e487bb12700b9550adb61e3c66cfdf7b295c2c
dhy <1824266713@qq.com>
2023-05-24 22:56:24 +0800

    自定义table测试

--- src/views/list/index.vue
+++ src/views/list/index.vue
@@ -1,57 +1,104 @@
 <template>
   <div class="container">
-    <!-- <div v-for="item of list">
-      {{ item }}
-    </div> -->
-    <iframe :src="src"> IE：你们都看我干吗，我现在也是支持的 </iframe>
-  </div>
-  <div>
-    <el-button @click="go">跳转go</el-button>
+    <el-table :data="tableData">
+      <el-table-column type="index" :index="$index" label="序号" min-width="80"></el-table-column>
+      <el-table-column prop="id" label="id" v-if="showProps('id')" min-width="80"></el-table-column>
+      <el-table-column prop="name" label="名称" v-if="showProps('name')" min-width="80">
+        <template #header>
+          <div>
+            <span>名称</span>
+            <el-icon><Search /></el-icon>
+          </div>
+        </template>
+        <template #default="scope">
+          <div @click="canEdit(scope.row)" @mouseleave="notEdit(scope.row)">
+            <el-input v-model="scope.row.name" v-if="scope.row.isEdit"></el-input>
+            <span v-else>{{ scope.row.name }}</span>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column prop="like" label="喜欢" v-if="showProps('like')"></el-table-column>
+      <el-table-column fixed="right" :width="100">
+        <template #header>
+          <div>
+            <el-tooltip effect="light">
+              <template #content>
+                <el-checkbox-group v-model="tableCheckedProps">
+                  <el-checkbox
+                    v-for="item of tableProps"
+                    :key="item.value"
+                    :label="item.value"
+                    style="display: block; margin-top: 10px"
+                    >{{ item.text }}</el-checkbox
+                  >
+                </el-checkbox-group>
+              </template>
+              <el-icon><CaretBottom /></el-icon>
+            </el-tooltip>
+          </div>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-button @click="save">保存</el-button>
+    <el-table :data="newtableData">
+      <el-table-column type="index" :index="$index" label="序号" width="80"></el-table-column>
+      <el-table-column prop="id" label="id"></el-table-column>
+      <el-table-column prop="name" label="名称"> </el-table-column>
+      <el-table-column prop="like" label="喜欢"></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { Search, CaretBottom } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const tableProps = ref([
+  { text: 'id', value: 'id' },
+  { text: '名称', value: 'name' },
+  { text: '喜欢', value: 'like' }
+])
 
-const src =
-  'http://39.99.151.225:20001/icbc/#/dataCenter/loopDataCenter?params=%7B%22condition%22%3A%7B%22key%22%3A%22drama_id%22,%22label%22%3A%22%E5%85%B3%E8%81%94%E4%BF%A1%E6%81%AF%22,%22op%22%3A%22equals%22,%22value%22%3A%22%E5%87%AD%E8%AF%81%E6%9C%89%E9%97%A8%E7%A6%81%22,%22valueText%22%3A%22%E5%87%AD%E8%AF%81%E6%9C%89%E9%97%A8%E7%A6%81%22%7D,%22startTime%22%3A%222023-03-17%2011%3A44%3A42%22,%22endTime%22%3A%222023-03-17%2011%3A41%3A38%22%7D'
-const list = []
-const num = 100
-const initList = () => {
-  for (let i = 0; i < num; i++) {
-    list.push(i)
+const tableCheckedProps = ref(['id', 'name', 'like'])
+const tableData = ref([
+  {
+    id: 1,
+    name: '邓皓月',
+    like: '运动'
+  },
+  {
+    id: 2,
+    name: '邺琳玲',
+    like: '吃'
   }
+])
+const newtableData = ref([])
+const canEdit = (row) => {
+  row.isEdit = true
 }
-initList()
-const myScroll = () => {
-  const container = document.querySelectorAll('.container')
-  console.log('container', container)
-  container.addEventListener('scroll', () => {
-    const offset = container.scrollHeight - container.scrollTop
-    const delta = 50
 
-    console.log('offset', offset)
-
-    if (offset <= 500 + delta) {
-      // 可以设置滚动锁，锁定滚动监听
-
-      const newDom = document.createElement('div')
-      newDom.setAttribute('class', 'item')
-      newDom.innerText = '我是新加的'
-      container.appendChild(newDom)
-    }
-  })
+const notEdit = (row) => {
+  row.isEdit = false
 }
-const go = () => {
-  window.open('about:bank', '_top').close()
+
+const save = () => {
+  console.log('new', tableData.value)
+  newtableData.value = tableData.value
 }
-onMounted(() => {
-  // myScroll()
-})
+
+const filterHandler = (value, row, column) => {
+  console.log('filter', value, row, column)
+}
+
+const showProps = (prop) => {
+  return tableCheckedProps.value.includes(prop)
+}
 </script>
 <style lang="scss" scoped>
 .container {
   height: 500px;
   overflow: auto;
+}
+:deep(.el-checkbox) {
+  height: auto;
 }
 </style>
