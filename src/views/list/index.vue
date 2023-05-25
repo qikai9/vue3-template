a288e70c5302fa2ae81a691cb4fcca8cffe2cfaa
dhy <1824266713@qq.com>
2023-05-25 23:29:34 +0800

    列表勾选和反选

--- src/views/list/index.vue
+++ src/views/list/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
-    <el-table :data="tableData">
+    <el-checkbox v-model="oppositeCheck" @change="changeOpposite"> 反选</el-checkbox>
+    <el-table :data="tableData"  @selection-change="handleSelectionChange" ref="tableRef" row-key="id">
+      <el-table-column type="selection" width="55" :reserve-selection="true" />
       <el-table-column type="index" :index="$index" label="序号" min-width="80"></el-table-column>
       <el-table-column prop="id" label="id" v-if="showProps('id')" min-width="80"></el-table-column>
       <el-table-column prop="name" label="名称" v-if="showProps('name')" min-width="80">
@@ -51,7 +53,7 @@
 
 <script setup>
 import { Search, CaretBottom } from '@element-plus/icons-vue'
-import { ref } from 'vue'
+import { ref, onBeforeMount, onMounted } from 'vue'
 const tableProps = ref([
   { text: 'id', value: 'id' },
   { text: '名称', value: 'name' },
@@ -69,8 +71,80 @@ const tableData = ref([
     id: 2,
     name: '邺琳玲',
     like: '吃'
+  },
+  {
+    id: 1,
+    name: '邓皓月',
+    like: '运动'
+  },
+  {
+    id: 2,
+    name: '邺琳玲',
+    like: '吃'
+  },
+  {
+    id: 1,
+    name: '邓皓月',
+    like: '运动'
+  },
+  {
+    id: 2,
+    name: '邺琳玲',
+    like: '吃'
+  },
+  {
+    id: 3,
+    name: '邓皓月',
+    like: '运动'
+  },
+  {
+    id: 4,
+    name: '邺琳玲',
+    like: '吃'
+  },
+  {
+    id: 5,
+    name: '邓皓月',
+    like: '运动'
+  },
+  {
+    id: 6,
+    name: '邺琳玲',
+    like: '吃'
+  },
+  {
+    id: 7,
+    name: '邓皓月',
+    like: '运动'
+  },
+  {
+    id: 8,
+    name: '邺琳玲',
+    like: '吃'
+  },
+  {
+    id: 9,
+    name: '邓皓月',
+    like: '运动'
+  },
+  {
+    id: 10,
+    name: '邺琳玲',
+    like: '吃'
+  },
+  {
+    id: 11,
+    name: '邓皓月',
+    like: '运动'
+  },
+  {
+    id: 12,
+    name: '邺琳玲',
+    like: '吃'
   }
 ])
+
+const oppositeTableData = ref([])
 const newtableData = ref([])
 const canEdit = (row) => {
   row.isEdit = true
@@ -92,10 +166,50 @@ const filterHandler = (value, row, column) => {
 const showProps = (prop) => {
   return tableCheckedProps.value.includes(prop)
 }
+// 勾选的数据
+const selectionIds = ref([])
+const allIds = ref([])
+const handleSelectionChange = (row) => {
+  let ids= []
+  for(let item of row) {
+    ids.push(item.id)
+  }
+  selectionIds.value = ids
+}
+
+const oppositeCheck = ref(false)
+const changeOpposite = (val) => {
+  // 未勾选的数据
+  oppositeTableData.value = tableData.value.filter(item => {
+    return !selectionIds.value.includes(item.id)
+  })
+  // 取消所有勾选
+  tableRef.value.clearSelection()
+  // 勾选之前未勾选的数据
+  for(let item of oppositeTableData.value) {
+    tableRef.value.toggleRowSelection(item)
+  }
+}
+
+const initAllIds = () => {
+  let ids = []
+  for(let item of tableData.value) {
+    ids.push(item.name)
+  }
+  allIds.value = ids
+}
+
+const tableRef = ref()
+onBeforeMount(() => {
+  initAllIds()
+})
+
+onMounted(() => {
+  // tableRef.value.toggleRowSelection(tableData.value[0])
+})
 </script>
 <style lang="scss" scoped>
 .container {
-  height: 500px;
   overflow: auto;
 }
 :deep(.el-checkbox) {
